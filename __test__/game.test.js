06f74d79fe83a1bd49510ba0614535fdfa2a4908
vishnupriya <1412426+vishnupriya@users.noreply.github.com>
2023-10-27 13:00:09 +0100

    Update game.test.js

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -4,8 +4,8 @@ const Game = require('../src/game').default
 // describe('App', () => {
 //   it('Contains the compiled JavaScript', async (done) => {
 //     fs.readFile('./public/main.js', 'utf8', (err, data) => {
-      // expect(err).toBe(null)
-      // expect(data).toMatchSnapshot()
+//       expect(err).toBe(null)
+//       expect(data).toMatchSnapshot()
 //       done()
 //     })
 //   })
